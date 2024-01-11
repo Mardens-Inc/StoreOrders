359866ec0746737db0f80aa65790a93069d6f437
drew-chase <dcmanproductions@gmail.com>
2024-01-11 16:14:29 -0500

    Add new styles and navigation elements to index.php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="/assets/css/main.min.css">
     <link rel="stylesheet" href="/assets/css/theme.min.css">
     <link rel="stylesheet" href="/assets/css/fonts.min.css">
+    <link rel="stylesheet" href="/assets/css/scrollbar.min.css">
+    <link rel="stylesheet" href="/assets/css/input.min.css">
+    <link rel="stylesheet" href="/assets/css/nav.min.css">
+    <link rel="stylesheet" href="/assets/css/dashboard.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/assets/lib/fontawesome/css/all.min.css">
 
@@ -19,8 +23,33 @@
 </head>
 
 <body>
-    <h1>Hello World</h1>
-    <p>Other stuff</p>
+    <nav class="panel">
+        <div class="nav-header col">
+            <h1>Store<br>Orders</h1>
+            <div class="row">
+                <button class="primary outline">New Requests</button>
+                <a href="#">Settings</a>
+            </div>
+        </div>
+        <div class="nav-items">
+            <div class="nav-item"> <img src="/assets/images/icon/home.svg" active> Home</div>
+            <div class="nav-item"> <img src="/assets/images/icon/cart.svg"> Current Orders</div>
+            <div class="nav-item"> <img src="/assets/images/icon/list.svg"> Orders Pending</div>
+            <div class="nav-item"> <img src="/assets/images/icon/calendar-search.svg"> Order History</div>
+        </div>
+    </nav>
+    <div class="row">
+
+        <div class="topbar panel col">
+            <input type="search" place>
+        </div>
+
+        <main>
+
+        </main>
+
+    </div>
+    <script src="/assets/js/inputs.min.js"></script>
 </body>
 
 </html>
