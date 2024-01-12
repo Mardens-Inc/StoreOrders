efa7764996f50fbb5fa24ab03460f3b1f5003190
drew-chase <dcmanproductions@gmail.com>
2024-01-12 13:13:55 -0500

    Add topbar component with search area, notification area, and user profile dropdown

---
+++ assets/php/components/topbar.php
@@ -0,0 +1,23 @@
+<div id="topbar" class="panel col">
+    <div class="search-area">
+        <input type="search" placeholder="Search here...">
+    </div>
+    <div class="notification-area row">
+        <div id="notification" tabindex="-1">
+            <img src="/assets/images/icon/bell.svg">
+            <span class="badge">3</span>
+            <div class="drop-down col">
+                <p>No Notifications</p>
+            </div>
+        </div>
+        <div id="user-profile" tabindex="-1">
+            <img src="https://www.rd.com/wp-content/uploads/2019/11/cat-10-e1573844975155-scaled.jpg">
+            <span class="badge">3</span>
+            <div class="drop-down col">
+                <div class="drop-item" page="/account/profile">Profile</div>
+                <div class="drop-item" page="/account/settings">Settings</div>
+                <div class="drop-item" page="/account/logout">Logout</div>
+            </div>
+        </div>
+    </div>
+</div>
