73d878a091a680f2340242deb2227f141622e8be
drew-chase <dcmanproductions@gmail.com>
2024-01-12 13:13:48 -0500

    Add navigation component to PHP files

---
+++ assets/php/components/nav.php
@@ -0,0 +1,15 @@
+<nav class="panel">
+        <div class="nav-header col">
+            <h1>Store<br>Orders</h1>
+            <div class="row">
+                <button class="primary outline" page="/orders/new">New Requests</button>
+                <a href="javascript:void();" page="/settings/application">Settings</a>
+            </div>
+        </div>
+        <div class="nav-items">
+            <div class="nav-item" page="/"> <img src="/assets/images/icon/home.svg" active> Home</div>
+            <div class="nav-item" page="/orders/current"> <img src="/assets/images/icon/cart.svg"> Current Orders</div>
+            <div class="nav-item" page="/orders/pending"> <img src="/assets/images/icon/list.svg"> Orders Pending</div>
+            <div class="nav-item" page="/orders/history"> <img src="/assets/images/icon/calendar-search.svg"> Order History</div>
+        </div>
+    </nav>
