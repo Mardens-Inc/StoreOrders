f12e6405cbe9761761ab67e907042daff6881e65
drew-chase <dcmanproductions@gmail.com>
2024-01-15 08:07:44 -0500

    Add login page HTML and CSS files

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Store Orders</title>
+    <link rel="stylesheet" href="/assets/css/main.min.css">
+    <link rel="stylesheet" href="/assets/css/theme.min.css">
+    <link rel="stylesheet" href="/assets/css/fonts.min.css">
+    <link rel="stylesheet" href="/assets/css/scrollbar.min.css">
+    <link rel="stylesheet" href="/assets/css/input.min.css">
+    <link rel="stylesheet" href="/assets/css/nav.min.css">
+    <link rel="stylesheet" href="/assets/css/dashboard.min.css">
+
+    <!-- jQuery -->
+    <script src="/assets/lib/jquery.min.js"></script>
+    <link rel="icon" href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_32,h_32/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-32x32.png" sizes="32x32">
+    <link rel="icon" href="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_192,h_192/https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-192x192.png" sizes="192x192">
+
+</head>
+
+<body>
+    <script src="/assets/js/inputs.min.js"></script>
+
+</body>
+
+</html>
