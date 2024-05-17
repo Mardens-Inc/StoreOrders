f83a26ed5bfe9ca9a32c4232e1d0f0ec72f45c6a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-17 10:11:45 -0400

    Add ApplicationData class in Core Constants
    The new ApplicationData class has been created in the Core/Constants directory of the StoreOrders project. This class is responsible for providing various pieces of general application information, such as the application's name, uptime, main assembly, assembly name, version, and related data generation functionality.

---
+++ StoreOrders.Core/Constants/ApplicationData.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace StoreOrders.Core.Constants;
+
+public static class ApplicationData
+{
+    public static string ApplicationName { get; } = "Store Orders";
+    public static TimeSpan UpTime => DateTime.Now - Process.GetCurrentProcess().StartTime;
+    public static Assembly MainAssembly { get; } = Assembly.GetExecutingAssembly();
+    public static AssemblyName? AssemblyName { get; } = MainAssembly.GetName();
+    public static Version? Version { get; } = AssemblyName.Version;
+
+    public static object GenerateApplicationData()
+    {
+        return new
+        {
+            ApplicationName,
+            Version,
+            UpTime,
+            Process.GetCurrentProcess().StartTime,
+            Environment = "RELEASE",
+            Config = ApplicationConfiguration.Instance,
+        };
+    }
+}
