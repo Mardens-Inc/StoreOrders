0ada8cb8487529135919f34366083e5b7a2dcafd
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-17 10:11:05 -0400

    Add Files.cs to store file path constants
    This commit adds a new class named Files, in the Constants namespace, which includes static properties representing the file paths of application configuration and different types of logs (latest, error, debug). This promotes reusability of these values across the codebase.

---
+++ StoreOrders.Core/Constants/Files.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreOrders.Core.Constants;
+
+using static Directories;
+
+public class Files
+{
+    public static string ApplicationConfiguration { get; } = Path.Combine(Root, "application.json");
+    public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
+    public static string ErrorLog { get; } = Path.Combine(Logs, "error.log");
+    public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
+}
