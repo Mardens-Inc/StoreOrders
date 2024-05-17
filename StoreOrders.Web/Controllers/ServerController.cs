cf0d17ef7e7e4610b56e2ab4f5bd43407fdf5845
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-17 10:15:00 -0400

    Add ServerController in StoreOrders.Web
    Added a new ServerController in the StoreOrders.Web project. This controller defines a single Index action that returns an Ok status along with generated application data.

---
+++ StoreOrders.Web/Controllers/ServerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreOrders.Core.Constants;
+
+namespace StoreOrders.Web.Controllers;
+
+[ApiController]
+[Route("/api")]
+public class ServerController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok(ApplicationData.GenerateApplicationData());
+    }
+}
