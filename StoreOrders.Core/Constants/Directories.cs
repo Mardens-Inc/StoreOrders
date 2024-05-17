3639741123cb96f6cf3f58bceda8a4e798700573
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-05-17 10:11:20 -0400

    Added Directories class in Constants namespace
    A new static class called Directories has been added to the StoreOrders.Core.Constants namespace. This class contains properties for generating and storing necessary directory paths like Root, Logs, and ApplicationData.

---
+++ StoreOrders.Core/Constants/Directories.cs
@@ -0,0 +1,8 @@
+﻿namespace StoreOrders.Core.Constants;
+
+public static class Directories
+{
+    public static string Root { get; } = Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data")).FullName;
+    public static string Logs { get; } = Directory.CreateDirectory(Path.Combine(Root, "logs")).FullName;
+    public static string ApplicationData { get; } = Directory.CreateDirectory(Path.Combine(Root, "appdata")).FullName;
+}
