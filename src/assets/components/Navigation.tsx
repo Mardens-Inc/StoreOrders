bf89dd3612ae74336de7b39cebdd1bab751315eb
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-07 16:29:50 -0400

    Add Navigation component
    Introduce a new `Navigation` component featuring navbar functionality. This includes logo display, category selection dropdown, cart preview dropdown, and user profile dropdown.

---
+++ src/assets/components/Navigation.tsx
@@ -0,0 +1,68 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
+import {Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Image, User} from "@nextui-org/react";
+import logo from "../images/mardens-logo.svg";
+import {faChevronDown, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import NavigationDropdown, {NavigationDropdownItem} from "./NavigationDropdown.tsx";
+import Pants from "../images/Pants.svg.tsx";
+import grid from "../images/grid.svg";
+
+export default function Navigation()
+{
+
+
+    return (
+        <Navbar maxWidth={"full"} className={"bg-primary text-white"}>
+            <NavbarContent justify={"start"}>
+                <NavbarBrand className={"max-w-[200px] mr-4"}>
+                    <Image src={logo} radius={"none"} width={200}/>
+                </NavbarBrand>
+                <NavbarItem className={"h-full"}>
+                    <NavigationDropdown
+                        trigger={"categories"}
+                        title={
+                            <div className={"flex flex-col items-center justify-center text-center text-4xl uppercase"}>
+                                <p className={"font-thin text-[29.06px]"}>select a</p>
+                                <p className={"font-black text-[47px]"}>category</p>
+                            </div>
+                        }
+                        subtitle={<p className={"text-center"}>To find your item by category,<br/>select a category here</p>}
+                    >
+                        <NavigationDropdownItem icon={<Pants/>}>Clothing</NavigationDropdownItem>
+                    </NavigationDropdown>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarContent justify="end">
+                <NavbarItem className={"h-full"}>
+                    <NavigationDropdown
+                        trigger={<><FontAwesomeIcon icon={faShoppingCart}/> cart</>}
+                        title={
+                            <div className={"flex flex-col items-center justify-center text-center text-4xl uppercase"}>
+                                <p className={"font-thin text-[29.06px]"}>view your</p>
+                                <p className={"font-black text-[47px]"}>cart</p>
+                            </div>
+                        }
+                        subtitle={<p className={"text-center"}>To find your item by category,<br/>select a category here</p>}
+                    >
+                        {[...Array.from({length: 11}, (_, i) => i).map((i) => (
+                            <NavigationDropdownItem>Item {i}</NavigationDropdownItem>
+                        )), <NavigationDropdownItem icon={<Image src={grid} radius={"none"} width={64}/>}>Checkout</NavigationDropdownItem>]}
+                    </NavigationDropdown>
+                </NavbarItem>
+                <NavbarItem>
+                    <Dropdown classNames={{content: "rounded-none w-full"}}>
+                        <DropdownTrigger>
+                            <div className={"flex flex-row cursor-pointer text-white items-center gap-8 hover:bg-black/10 p-3 transition-colors"}>
+                                <User name={"Waterville Store"} className={"uppercase"} classNames={{name: "text-tiny font-bold"}}/>
+                                <FontAwesomeIcon icon={faChevronDown} width={12}/>
+                            </div>
+                        </DropdownTrigger>
+                        <DropdownMenu itemClasses={{base: "rounded-none"}}>
+                            <DropdownItem>hi</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>);
+}
