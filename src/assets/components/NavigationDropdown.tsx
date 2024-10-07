c64b1449fb13c60bdc71e1d8b4679d259d8d176b
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-07 16:29:40 -0400

    Add NavigationDropdown component
    Introduce NavigationDropdown and NavigationDropdownItem components to support dropdown navigation menus. These components utilize React state for toggling visibility and integrate styles from FontAwesome and NextUI for an enhanced user experience.

---
+++ src/assets/components/NavigationDropdown.tsx
@@ -0,0 +1,72 @@
+import {ReactNode, useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
+import SquareBackdrop from "../images/SquareBackdrop.svg.tsx";
+import $ from "jquery";
+import {cn} from "@nextui-org/react";
+
+interface NavigationDropdownProps
+{
+    trigger: string | ReactNode;
+    title?: string | ReactNode;
+    subtitle?: string | ReactNode;
+    children: ReactNode[] & { type: typeof NavigationDropdownItem }[] | ReactNode & { type: typeof NavigationDropdownItem };
+}
+
+export default function NavigationDropdown(props: NavigationDropdownProps)
+{
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className={"h-full"}>
+            <div className={"flex flex-row uppercase font-bold cursor-pointer hover:bg-black/10 transition-colors h-full items-center px-4"} onClick={() => setIsOpen(prev => !prev)}>
+                <span className={"mr-8"}>{props.trigger}</span>
+                <FontAwesomeIcon icon={faChevronDown} width={14} data-open={isOpen} className={"data-[open=true]:rotate-180 transition-all"}/>
+            </div>
+            <div className={"w-screen max-h-[370px] h-[370px] bg-white border-b-primary border-b-2 shadow-lg fixed left-0 right-0 flex flex-row data-[open=false]:max-h-0 transition-all overflow-hidden"} data-open={isOpen}>
+                <div className={"min-w-[480px] w-[480px] bg-primary relative flex flex-col items-center justify-center"}>
+                    <div className={"z-10 flex flex-col gap-4"}>
+                        {props.title}
+                        {props.subtitle}
+                    </div>
+                    <div className={"absolute left-0 right-0 top-0 bottom-0"}><SquareBackdrop/></div>
+                </div>
+                <div className={"flex flex-col flex-wrap justify-start items-start gap-3 text-black overflow-x-auto w-full"}>
+                    {props.children}
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export function NavigationDropdownItem(props: { children: ReactNode, icon?: ReactNode })
+{
+    const [hovering, setHovering] = useState(false);
+    const key = `navigation-dropdown-item-${Math.random().toString(36).substring(7)}`;
+    useEffect(() =>
+    {
+        $(`#${key}`)
+            .on("mouseenter", () => setHovering(true))
+            .on("mouseleave", () => setHovering(false));
+    }, []);
+    return (
+        <div
+            id={key}
+            className={
+                cn(
+                    "flex flex-col gap-4 items-center justify-center w-[150px] h-[160px]",
+                    "bg-white p-4 cursor-pointer uppercase font-bold text-[#221E20]",
+                    "data-[hovering=true]:outline-primary outline-2 outline outline-transparent data-[hovering=true]:bg-primary/10"
+                )
+            }
+            data-hovering={hovering}>
+            {props.icon && (
+                <div className={"data-[hovering=true]:animate-[wiggle_1s_ease-in-out_infinite] data-[hovering=true]:scale-75 transform-none transition-all"} data-hovering={hovering}>
+                    {props.icon}
+                </div>
+
+            )}
+            {props.children}
+        </div>
+    );
+}
