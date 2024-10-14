ec291be9f0e0c24f23944eb18cbd5ae6d84c8618
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-14 10:53:16 -0400

    Replace Home with CatalogPage and add CartProvider
    Updated the main component to use CatalogPage instead of Home and wrapped MainContentRenderer with CartProvider. This change enhances the functionality by integrating cart capabilities and changing the default landing page.

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import $ from "jquery";
 import {NextUIProvider} from "@nextui-org/react";
 
 import "./assets/scss/index.scss";
-import Home from "./assets/pages/Home.tsx";
+import CatalogPage from "./assets/pages/CatalogPage.tsx";
 import Navigation from "./assets/components/Navigation.tsx";
 import {applyTheme} from "./assets/ts/Theme.ts";
+import {CartProvider} from "./assets/providers/CartProvider.tsx";
 
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <MainContentRenderer/>
+            <CartProvider>
+                <MainContentRenderer/>
+            </CartProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
@@ -27,7 +30,7 @@ export function MainContentRenderer()
             <Navigation/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Home/>}/>
+                    <Route path="/" element={<CatalogPage/>}/>
                 </Route>
             </Routes>
         </NextUIProvider>
