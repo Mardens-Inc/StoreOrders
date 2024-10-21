1af7e05d1b5d1e5aee0ee26dc8d7a7b265872f85
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-21 16:13:19 -0400

    Enhance Order History Page with actions and filters
    Added "View" and "Print" buttons for each order, enhanced status display with color-coded chips, and integrated filters for store, status, and week selection. Updated the page title to "Order History" and included necessary imports for new components and icons.

--- src/assets/pages/OrderHistoryPage.tsx
+++ src/assets/pages/OrderHistoryPage.tsx
@@ -1,16 +1,20 @@
-import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
+import {Button, Chip, DateValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
 import {useEffect, useState} from "react";
 import OrderManagement, {getWeekNumber, Orders, OrderStatus} from "../ts/Orders.ts";
 import StoreFilterDropdown from "../components/OrderHistory/StoreFilterDropdown.tsx";
 import OrderStatusFilterDropdown from "../components/OrderHistory/OrderStatusFilterDropdown.tsx";
 import WeekFilterDropdown from "../components/OrderHistory/WeekFilterDropdown.tsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faPrint} from "@fortawesome/free-solid-svg-icons";
+import {setTitle} from "../../main.tsx";
 
 export default function OrderHistoryPage()
 {
+    setTitle("Order History");
     const [orders, setOrders] = useState<Orders[]>([]);
     const [filterByStores, setFilterByStores] = useState<string[] | "all">("all");
     const [filterByStatus, setFilterByStatus] = useState<OrderStatus[] | "all">("all");
-    // const [filterByWeek, setFilterByWeek] = useState<number | "all">("all");
+    const [filterByWeek, setFilterByWeek] = useState<DateValue | null>(null);
 
 
     useEffect(() =>
@@ -22,22 +26,45 @@ export default function OrderHistoryPage()
         <div className={"flex flex-col mx-10 mt-4"}>
             <h1 className={"text-4xl font-bold"}>Order History</h1>
             <p className={"italic mb-4"}>Here you can view your order history.</p>
-            <Table removeWrapper aria-label="Order history table">
+            <Table removeWrapper aria-label="Order history table" selectionMode={"single"}>
                 <TableHeader>
-                    <TableColumn allowsSorting key={"week"}>Week</TableColumn>
-                    <TableColumn allowsSorting key={"week-start"}>Week Starting <WeekFilterDropdown/></TableColumn>
                     <TableColumn allowsSorting key={"order-number"}>Order#</TableColumn>
+                    <TableColumn allowsSorting key={"week"}>Week</TableColumn>
+                    <TableColumn allowsSorting key={"week-start"}>Week Starting <WeekFilterDropdown value={filterByWeek} onValueChange={setFilterByWeek}/></TableColumn>
                     <TableColumn allowsSorting key={"store"}>Store <StoreFilterDropdown value={filterByStores} onValueChange={setFilterByStores}/></TableColumn>
                     <TableColumn allowsSorting key={"status"}>Status <OrderStatusFilterDropdown value={filterByStatus} onValueChange={setFilterByStatus}/></TableColumn>
+                    <TableColumn key={"actions"} className={"w-0"}>Actions</TableColumn>
                 </TableHeader>
                 <TableBody>
                     {orders.map((order) => (
                         <TableRow key={order.id}>
+                            <TableCell>{order.id}</TableCell>
                             <TableCell>{getWeekNumber(order.week)}</TableCell>
                             <TableCell>{order.week.toDateString()}</TableCell>
-                            <TableCell>{order.id}</TableCell>
                             <TableCell>{order.store.name}</TableCell>
-                            <TableCell className={"capitalize"}>{order.status}</TableCell>
+                            <TableCell className={"capitalize"}>
+                                {(() =>
+                                    {
+                                        switch (order.status)
+                                        {
+                                            case OrderStatus.CURRENT:
+                                                return <Chip color={"primary"} variant={"shadow"} className={"max-w-[unset] min-w-24"}>Current</Chip>;
+                                            case OrderStatus.COMPLETED:
+                                                return <Chip color={"success"} variant={"shadow"} className={"max-w-[unset] min-w-24"}>Completed</Chip>;
+                                            case OrderStatus.PENDING:
+                                                return <Chip color={"warning"} variant={"shadow"} className={"max-w-[unset] min-w-24"}>Pending</Chip>;
+                                            case OrderStatus.CANCELLED:
+                                                return <Chip color={"danger"} variant={"shadow"} className={"max-w-[unset] min-w-24"}>Cancelled</Chip>;
+                                        }
+                                    }
+                                )()}
+                            </TableCell>
+                            <TableCell>
+                                <div className={"flex flex-row gap-2"}>
+                                    <Button variant={"light"} className={"min-w-0"}><FontAwesomeIcon icon={faEye}/></Button>
+                                    <Button variant={"light"} className={"min-w-0"}><FontAwesomeIcon icon={faPrint}/></Button>
+                                </div>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
