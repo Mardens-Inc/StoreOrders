f579ff5bcce0f61750f713a02a27ff7e9ebe76bc
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-21 16:13:39 -0400

    Add cart management functions to CartProvider
    Implemented `addProduct`, `removeProduct`, and `clearCart` functions to manage the cart state within `CartProvider`. Enhanced the cart to maintain product quantities and enforce type safety using the new `CartItem` interface.

--- src/assets/providers/CartProvider.tsx
+++ src/assets/providers/CartProvider.tsx
@@ -1,18 +1,50 @@
 import {createContext, Dispatch, ReactNode, SetStateAction, useContext, useState} from "react";
+import {Product} from "../ts/Products.ts";
+
+export interface CartItem extends Product
+{
+    quantity: number;
+}
 
 interface CartContextType
 {
-    cart: number[];
-    setCart: Dispatch<SetStateAction<number[]>>;
+    cart: CartItem[];
+    setCart: Dispatch<SetStateAction<CartItem[]>>;
+    addProduct: (product: Product, quantity: number) => void;
+    removeProduct: (product: Product) => void;
+    clearCart: () => void;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export function CartProvider({children}: { children: ReactNode })
 {
-    const [cart, setCart] = useState<number[]>([]);
+    const [cart, setCart] = useState<CartItem[]>([]);
     return (
-        <CartContext.Provider value={{cart, setCart}}>
+        <CartContext.Provider value={
+            {
+                cart, setCart,
+                addProduct: (product: Product, quantity: number) =>
+                {
+                    const item = cart.find((item) => item.id === product.id);
+                    if (item)
+                    {
+                        item.quantity += quantity;
+                        setCart([...cart]);
+                    } else
+                    {
+                        setCart([...cart, {...product, quantity}]);
+                    }
+                },
+                removeProduct: (product: Product) =>
+                {
+                    setCart(cart.filter((item) => item.id !== product.id));
+                },
+                clearCart: () =>
+                {
+                    setCart([]);
+                }
+            }}>
             {children}
         </CartContext.Provider>
     );
