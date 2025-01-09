365e1721231156614b9f847b9b20207ab1918a2b
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-09 08:26:29 -0500

    Refactor main application structure and add Vite integration.
    Modularized the code by introducing new modules for better separation of concerns. Added functionality to initialize database connections and integrate a Vite development server in development mode. Improved error handling, payload processing, and overall code readability.

--- src-actix/main.rs
+++ src-actix/main.rs
@@ -1,63 +1,148 @@
-use actix_web::web::trace;
-use actix_web::{get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+mod data_database_connection;
+mod data_orders;
+mod data_product;
+mod db_orders;
+mod db_product;
+mod traits;
+
+use crate::data_database_connection::DatabaseConnectionData;
+use actix_web::{
+	get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use awc::Client;
 use futures_util::stream::StreamExt;
 use include_dir::{include_dir, Dir};
-use log::{debug, error, info, trace, warn};
+use log::{debug, error, info};
 use serde_json::json;
+use std::error::Error as StdError;
+use std::process::Child;
+
+const DEBUG: bool = cfg!(debug_assertions);
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-	std::env::set_var("RUST_LOG", "trace");
-	env_logger::init();
+async fn main() -> Result<(), Box<dyn StdError>> {
+    std::env::set_var("RUST_LOG", "trace");
+    env_logger::init();
 
-	let config = if cfg!(debug_assertions) {
-		"development"
+	let data: Option<DatabaseConnectionData> = if DEBUG {
+		None
 	} else {
-		"production"
+		Some(DatabaseConnectionData::get().await?)
 	};
+	let data: Option<&DatabaseConnectionData> = data.as_ref();
 
-	let port = 1420; // Port to listen on
-	info!("Starting {} server at http://127.0.0.1:{}", config, port);
+    // Initialize the orders database
+    db_orders::initialize(data).await?;
 
-	HttpServer::new(move || {
-		let app = App::new()
-			.wrap(middleware::Logger::default())
-			.app_data(
-				web::JsonConfig::default()
-					.limit(4096)
-					.error_handler(|err, _req| {
-						let error = json!({ "error": format!("{}", err) });
-						actix_web::error::InternalError::from_response(
-							err,
-							HttpResponse::BadRequest().json(error),
-						).into()
-					})
-			)
-			.service(web::scope("api").service(status));
+    let config = if DEBUG { "development" } else { "production" };
 
-		// Add conditional routing based on the config
-		if config == "development" {
-			app.default_service(web::route().to(proxy_to_vite))
-			   .service(
-				   web::resource("/assets/{file:.*}")
-					   .route(web::get().to(proxy_to_vite))
-			   )
-			   .service(
-				   web::resource("/node_modules/{file:.*}")
-					   .route(web::get().to(proxy_to_vite))
-			   )
-		} else {
-			app.default_service(web::route().to(index))
-			   .service(web::resource("/assets/{file:.*}").route(web::get().to(index)))
-		}
-	})
-		.workers(4)
-		.bind(format!("0.0.0.0:{port}", port = port))?
-		.run()
-		.await
+    let port = 1424; // Port to listen on
+    let server = HttpServer::new(move || {
+        let app = App::new()
+            .wrap(middleware::Logger::default())
+            .app_data(
+                web::JsonConfig::default()
+                    .limit(4096)
+                    .error_handler(|err, _req| {
+                        let error = json!({ "error": format!("{}", err) });
+                        actix_web::error::InternalError::from_response(
+                            err,
+                            HttpResponse::BadRequest().json(error),
+                        )
+                        .into()
+                    }),
+            )
+            .service(web::scope("api").service(status));
+
+        // Add conditional routing based on the config
+        if DEBUG {
+            app.default_service(web::route().to(proxy_to_vite))
+                .service(web::resource("/assets/{file:.*}").route(web::get().to(proxy_to_vite)))
+                .service(
+                    web::resource("/node_modules/{file:.*}").route(web::get().to(proxy_to_vite)),
+                )
+        } else {
+            app.default_service(web::route().to(index))
+                .service(web::resource("/assets/{file:.*}").route(web::get().to(index)))
+        }
+    })
+    .workers(4)
+    .bind(format!("0.0.0.0:{port}", port = port))?
+    .run();
+    info!("Starting {} server at http://127.0.0.1:{}...", config, port);
+    server.await?;
+
+    if DEBUG {
+        start_vite_server().expect("Failed to start vite server");
+    }
+
+    Ok(())
 }
+/// Starts the Vite development server.
+///
+/// This function attempts to locate the `vite` executable using the appropriate
+/// system command (`where` on Windows and `which` on other systems). If the `vite`
+/// executable is found, it starts the Vite server in a new process. If the `vite`
+/// executable is not found, it returns an error.
+///
+/// # Returns
+///
+/// Returns a `Result` containing the `Child` process of the started Vite server,
+/// or an error if the executable is not found or the server fails to start.
+///
+/// # Errors
+///
+/// Returns an error if the `vite` executable is not found or if the command fails
+/// to start.
+///
+/// # Example
+///
+/// ```rust
+/// match start_vite_server() {
+///     Ok(child) => println!("Vite server started with PID: {}", child.id()),
+///     Err(e) => eprintln!("Failed to start Vite server: {}", e),
+/// }
+/// ```
+fn start_vite_server() -> Result<Child, Box<dyn StdError>> {
+    #[cfg(target_os = "windows")]
+    let find_cmd = "where";
+    #[cfg(not(target_os = "windows"))]
+    let find_cmd = "which";
 
+    let vite = std::process::Command::new(find_cmd)
+        .arg("vite")
+        .stdout(std::process::Stdio::piped())
+        .output()?
+        .stdout;
+
+    let vite = String::from_utf8(vite);
+    let vite = vite.unwrap();
+    let vite = vite.as_str().trim();
+
+    if vite.is_empty() {
+        error!("vite not found, make sure its installed with npm install -g vite");
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "vite not found",
+        ))?;
+    }
+
+    // Get the first occurrence
+    let vite = vite
+        .split("\n")
+        .collect::<Vec<_>>()
+        .last()
+        .expect("Failed to get vite executable")
+        .trim();
+
+    debug!("found vite at: {:?}", vite);
+
+    // Start the vite server
+    Ok(std::process::Command::new(vite)
+        .current_dir(r#"../../"#)
+        .spawn()
+        .expect("Failed to start vite server"))
+}
 
 /// The maximum payload size allowed for forwarding requests and responses.
 ///
@@ -86,11 +171,13 @@ static WWWROOT: Dir = include_dir!("target/wwwroot");
 /// An `impl Responder` which can either be a successful HTTP response containing
 /// the `index.html` file, or an internal server error.
 async fn index(_req: HttpRequest) -> Result<impl Responder, Error> {
-	if let Some(file) = WWWROOT.get_file("index.html") {
-		let body = file.contents();
-		return Ok(HttpResponse::Ok().content_type("text/html").body(body));
-	}
-	Err(actix_web::error::ErrorInternalServerError("Failed to find index.html"))
+    if let Some(file) = WWWROOT.get_file("index.html") {
+        let body = file.contents();
+        return Ok(HttpResponse::Ok().content_type("text/html").body(body));
+    }
+    Err(actix_web::error::ErrorInternalServerError(
+        "Failed to find index.html",
+    ))
 }
 
 /// Proxies requests to the Vite development server.
@@ -109,54 +196,52 @@ async fn index(_req: HttpRequest) -> Result<impl Responder, Error> {
 /// An `HttpResponse` which contains the response from the Vite server,
 /// or an error response in case of failure.
 async fn proxy_to_vite(req: HttpRequest, mut payload: web::Payload) -> Result<HttpResponse, Error> {
-	let client = Client::new();
-	let forward_url = format!("http://localhost:3000{}", req.uri());
+    let client = Client::new();
+    let forward_url = format!("http://localhost:3000{}", req.uri());
 
-	// Buffer the entire payload
-	let mut body_bytes = web::BytesMut::new();
-	while let Some(chunk) = payload.next().await {
-		let chunk = chunk?;
-		if (body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
-			return Err(actix_web::error::ErrorPayloadTooLarge(
-				"Payload overflow",
-			));
-		}
-		body_bytes.extend_from_slice(&chunk);
-	}
+    // Buffer the entire payload
+    let mut body_bytes = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        if (body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
+            return Err(actix_web::error::ErrorPayloadTooLarge("Payload overflow"));
+        }
+        body_bytes.extend_from_slice(&chunk);
+    }
 
-	let mut forwarded_resp = client
-		.request_from(forward_url.as_str(), req.head())
-		.no_decompress()
-		.send_body(body_bytes)
-		.await
-		.map_err(|err| {
-			actix_web::error::ErrorInternalServerError(format!(
-				"Failed to forward request: {}",
-				err
-			))
-		})?;
+    let mut forwarded_resp = client
+        .request_from(forward_url.as_str(), req.head())
+        .no_decompress()
+        .send_body(body_bytes)
+        .await
+        .map_err(|err| {
+            actix_web::error::ErrorInternalServerError(format!(
+                "Failed to forward request: {}",
+                err
+            ))
+        })?;
 
-	// Buffer the entire response body
-	let mut resp_body_bytes = web::BytesMut::new();
-	while let Some(chunk) = forwarded_resp.next().await {
-		let chunk = chunk?;
-		if (resp_body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
-			return Err(actix_web::error::ErrorPayloadTooLarge(
-				"Response payload overflow",
-			));
-		}
-		resp_body_bytes.extend_from_slice(&chunk);
-	}
+    // Buffer the entire response body
+    let mut resp_body_bytes = web::BytesMut::new();
+    while let Some(chunk) = forwarded_resp.next().await {
+        let chunk = chunk?;
+        if (resp_body_bytes.len() + chunk.len()) > MAX_PAYLOAD_SIZE {
+            return Err(actix_web::error::ErrorPayloadTooLarge(
+                "Response payload overflow",
+            ));
+        }
+        resp_body_bytes.extend_from_slice(&chunk);
+    }
 
-	// Build the response
-	let mut res = HttpResponse::build(forwarded_resp.status());
+    // Build the response
+    let mut res = HttpResponse::build(forwarded_resp.status());
 
-	// Copy headers
-	for (header_name, header_value) in forwarded_resp.headers().iter() {
-		res.insert_header((header_name.clone(), header_value.clone()));
-	}
+    // Copy headers
+    for (header_name, header_value) in forwarded_resp.headers().iter() {
+        res.insert_header((header_name.clone(), header_value.clone()));
+    }
 
-	Ok(res.body(resp_body_bytes))
+    Ok(res.body(resp_body_bytes))
 }
 
 /// Handles requests to check the server status.
@@ -170,5 +255,5 @@ async fn proxy_to_vite(req: HttpRequest, mut payload: web::Payload) -> Result<Ht
 /// A JSON object with a `status` field set to "ok".
 #[get("/")]
 async fn status() -> impl Responder {
-	HttpResponse::Ok().json(json!({ "status": "ok" }))
+    HttpResponse::Ok().json(json!({ "status": "ok" }))
 }
