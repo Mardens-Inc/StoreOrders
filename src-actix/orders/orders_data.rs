a2e5b53eb26692c0173ce8a83f0aeea575f9cd84
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-03 14:54:47 -0400

    Add `StoreOrderRecord` struct with SQLx integration for order data modeling

---
+++ src-actix/orders/orders_data.rs
@@ -0,0 +1,14 @@
+use crate::orders::store_order_status::StoreOrderStatus;
+use serde_hash::HashIds;
+use sqlx::FromRow;
+
+#[derive(HashIds, Debug, Clone, FromRow)]
+pub struct StoreOrderRecord {
+    #[hash]
+    pub id: u64,
+    pub order_number: u64,
+    pub status: StoreOrderStatus,
+    pub created_at: chrono::NaiveDateTime,
+    pub status_changed_to_pending: Option<chrono::NaiveDateTime>,
+    pub status_changed_to_completed: Option<chrono::NaiveDateTime>,
+}
